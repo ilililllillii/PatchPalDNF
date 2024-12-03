7e00bb054eba5ae441bcd49be01d00edf9d8d0d0
ilililllillii <1019766700@qq.com>
2024-12-03 13:25:30 +0800

    添加鼠标

--- PatchPalDNF/App.xaml.cs
+++ PatchPalDNF/App.xaml.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Resources;
 
 namespace PatchPalDNF
 {
@@ -14,5 +16,31 @@ namespace PatchPalDNF
     /// </summary>
     public partial class App : Application
     {
+        // 在应用启动时设置光标
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            try
+            {
+                // 设置全局光标为自定义光标
+                StreamResourceInfo streamInfo = GetResourceStream(new Uri("Resources/basic.cur", UriKind.Relative));
+                Mouse.OverrideCursor = new Cursor(streamInfo.Stream);
+            }
+            catch (Exception ex)
+            {
+                // 捕获异常并输出错误信息
+                MessageBox.Show($"错误: {ex.Message}");
+            }
+        }
+
+        // 在应用程序退出时恢复默认光标（可选）
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            // 恢复默认光标
+            Mouse.OverrideCursor = null;
+        }
     }
 }
